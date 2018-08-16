97e94113ad4e1a48965917c8a76c8c30b1660c50
bencso <gabor.bencsik13@gmail.com>
2018-08-16 12:45:43 +0200

    INT-0053: Catch and log application errors

---
+++ kong/plugins/header-based-rate-limiting/access.lua
@@ -0,0 +1,47 @@
+local responses = require "kong.tools.responses"
+
+local ConsumerIdentifier = require "kong.plugins.header-based-rate-limiting.consumer_identifier"
+local RateLimitKey = require "kong.plugins.header-based-rate-limiting.rate_limit_key"
+local RateLimitPool = require "kong.plugins.header-based-rate-limiting.rate_limit_pool"
+local RedisFactory = require "kong.plugins.header-based-rate-limiting.redis_factory"
+local Logger = require "logger"
+
+local RATE_LIMIT_HEADER = "X-RateLimit-Limit"
+local REMAINING_REQUESTS_HEADER = "X-Ratelimit-Remaining"
+local POOL_RESET_HEADER = "X-Ratelimit-Reset"
+
+local Access = {}
+
+function Access.execute(conf)
+    local redis = RedisFactory.create(conf.redis)
+    local pool = RateLimitPool(redis)
+    local actual_time = os.time()
+    local time_reset = os.date("!%Y-%m-%dT%H:%M:00Z", actual_time + 60)
+    local identifier = ConsumerIdentifier.generate(conf.identification_headers, ngx.req.get_headers())
+
+    local rate_limit_key = RateLimitKey.generate(identifier, conf, actual_time)
+
+    local request_count = pool:request_count(rate_limit_key)
+
+    if not conf.log_only then
+        ngx.header[RATE_LIMIT_HEADER] = conf.default_rate_limit
+        ngx.header[REMAINING_REQUESTS_HEADER] = conf.default_rate_limit - (request_count + 1)
+        ngx.header[POOL_RESET_HEADER] = time_reset
+    end
+
+    if request_count >= conf.default_rate_limit then
+        if not conf.log_only then
+            responses.send(429, "Rate limit exceeded")
+        end
+
+        Logger.getInstance(ngx):logInfo({
+            ["msg"] = "Rate limit exceeded",
+            ["uri"] = ngx.var.request_uri,
+            ["identifier"] = identifier
+        })
+    else
+        pool:increment(rate_limit_key)
+    end
+end
+
+return Access
--- kong/plugins/header-based-rate-limiting/handler.lua
+++ kong/plugins/header-based-rate-limiting/handler.lua
@@ -1,14 +1,6 @@
 local BasePlugin = require "kong.plugins.base_plugin"
-local responses = require "kong.tools.responses"
-
-local ConsumerIdentifier = require "kong.plugins.header-based-rate-limiting.consumer_identifier"
-local RateLimitKey = require "kong.plugins.header-based-rate-limiting.rate_limit_key"
-local RateLimitPool = require "kong.plugins.header-based-rate-limiting.rate_limit_pool"
-local RedisFactory = require "kong.plugins.header-based-rate-limiting.redis_factory"
-
-local RATE_LIMIT_HEADER = "X-RateLimit-Limit"
-local REMAINING_REQUESTS_HEADER = "X-Ratelimit-Remaining"
-local POOL_RESET_HEADER = "X-Ratelimit-Reset"
+local Access = require "kong.plugins.header-based-rate-limiting.access"
+local Logger = require "logger"
 
 local HeaderBasedRateLimitingHandler = BasePlugin:extend()
 
@@ -21,37 +13,10 @@ end
 function HeaderBasedRateLimitingHandler:access(conf)
     HeaderBasedRateLimitingHandler.super.access(self)
 
-    local success, result = pcall(RedisFactory.create, conf.redis)
+    local success, error = pcall(Access.execute, conf)
 
-    if success then
-        local redis = result
-        local pool = RateLimitPool(redis)
-        local actual_time = os.time()
-        local time_reset = os.date("!%Y-%m-%dT%H:%M:00Z", actual_time + 60)
-
-        local rate_limit_key = RateLimitKey.generate(
-        ConsumerIdentifier.generate(
-                conf.identification_headers, ngx.req.get_headers()),
-                conf,
-                actual_time
-        )
-
-        local request_count = pool:request_count(rate_limit_key)
-
-
-        if not conf.log_only then
-            ngx.header[RATE_LIMIT_HEADER] = conf.default_rate_limit
-            ngx.header[REMAINING_REQUESTS_HEADER] = conf.default_rate_limit - (request_count + 1)
-            ngx.header[POOL_RESET_HEADER] = time_reset
-        end
-
-        if request_count >= conf.default_rate_limit then
-            if not conf.log_only then
-                responses.send(429, "Rate limit exceeded")
-            end
-        else
-            pool:increment(rate_limit_key)
-        end
+    if not success then
+        Logger.getInstance(ngx):logError(error)
     end
 end
 

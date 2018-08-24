4be678984ec05c8c335a8b7d292f261ee58c95ee
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-08-24 07:37:39 +0200

    INT-0055: Admin API endpoint for adding rate limit settings

--- kong/plugins/header-based-rate-limiting/api.lua
+++ kong/plugins/header-based-rate-limiting/api.lua
@@ -3,6 +3,48 @@ local RedisFactory = require "kong.plugins.header-based-rate-limiting.redis_fact
 local utils = require "kong.tools.utils"
 local base64 = require "base64"
 
+local function decode_header_composition(header_based_rate_limit)
+    local result = {}
+
+    for key, value in pairs(header_based_rate_limit) do
+        if key == "header_composition" then
+            local individual_headers = utils.split(header_based_rate_limit.header_composition, ":")
+
+            local decoded_headers = {}
+
+            for _, header in ipairs(individual_headers) do
+                table.insert(decoded_headers, base64.decode(header))
+            end
+
+            result["header_composition"] = decoded_headers
+        else
+            result[key] = value
+        end
+    end
+
+    return result
+end
+
+local function encode_header_composition(header_based_rate_limit)
+    local result = {}
+
+    for key, value in pairs(header_based_rate_limit) do
+        if key == "header_composition" then
+            local encoded_headers = {}
+
+            for _, header in ipairs(value) do
+                table.insert(encoded_headers, base64.encode(header))
+            end
+
+            result["header_composition"] = table.concat(encoded_headers, ":")
+        else
+            result[key] = value
+        end
+    end
+
+    return result
+end
+
 return {
     ["/plugins/:plugin_id/redis-ping"] = {
         before = function(self, dao_factory, helpers)
@@ -30,31 +72,11 @@ return {
 
     ['/header-based-rate-limits'] = {
         POST = function(self, dao_factory, helpers)
+            local params_with_encoded_header_composition = encode_header_composition(self.params)
+            crud.post(params_with_encoded_header_composition, dao_factory.header_based_rate_limits, decode_header_composition)
         end,
 
         GET = function(self, dao_factory, helpers)
-            local function decode_header_composition(header_based_rate_limit)
-                local result = {}
-
-                for key, value in pairs(header_based_rate_limit) do
-                    if key == "header_composition" then
-                        local individual_headers = utils.split(header_based_rate_limit.header_composition, ":")
-
-                        local decoded_headers = {}
-
-                        for _, header in ipairs(individual_headers) do
-                            table.insert(decoded_headers, base64.decode(header))
-                        end
-
-                        result["header_composition"] = decoded_headers
-                    else
-                        result[key] = value
-                    end
-                end
-
-                return result
-            end
-
             crud.paginated_set(self, dao_factory.header_based_rate_limits, decode_header_composition)
         end
     }

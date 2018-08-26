67f1302163a8e6fde43962054057bd9a42fc8ea5
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-08-26 11:24:17 +0200

    INT-0055: Code reformat

--- kong/plugins/header-based-rate-limiting/rate_limit_key.lua
+++ kong/plugins/header-based-rate-limiting/rate_limit_key.lua
@@ -1,7 +1,11 @@
 local RateLimitKey = {}
 
 function RateLimitKey.generate(customer_identifier, config, actual_time)
-    return "ratelimit:" .. customer_identifier .. ":" .. (config.service_id or "") .. ":" .. (config.route_id or "") .. ":" .. os.date("!%Y%m%dT%H%M00Z", actual_time)
+    return "ratelimit:" ..
+        customer_identifier .. ":" ..
+        (config.service_id or "") .. ":" ..
+        (config.route_id or "") .. ":" ..
+        os.date("!%Y%m%dT%H%M00Z", actual_time)
 end
 
 return RateLimitKey

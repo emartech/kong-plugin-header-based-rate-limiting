63589081c62957858b24720ae269cb60a2852501
bencso <gabor.bencsik13@gmail.com>
2020-04-28 15:16:18 +0200

    CDP-101: Add migration to support composite cache key

---
+++ kong/plugins/header-based-rate-limiting/migrations/002_200_to_201.lua
@@ -0,0 +1,17 @@
+return {
+    postgres = {
+        up = [[
+            DO $$
+            BEGIN
+            UPDATE header_based_rate_limits SET cache_key = CONCAT('header_based_rate_limits', ':', service_id, ':', route_id, ':', header_composition, ':', ':') WHERE cache_key is null;
+            END;
+            $$;
+        ]]
+    },
+    cassandra = {
+        up = [[
+            ALTER TABLE header_based_rate_limits ADD cache_key text;
+            CREATE INDEX IF NOT EXISTS ON header_based_rate_limits (cache_key);
+        ]]
+    }
+}
